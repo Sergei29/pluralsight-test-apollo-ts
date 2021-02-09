c42717f6dfbee8cfca4b55ed51350704bb91c8e6
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-02-09 20:00:20 +0000

    basic apollo server setup

---
+++ server.ts
@@ -0,0 +1,19 @@
+import { gql, ApolloServer, ServerInfo } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    greeting: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    greeting: () => "Hello GraphQL World!",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 9000 }).then(({ url }: ServerInfo) => {
+  console.log(`Server running at ${url}`);
+});
